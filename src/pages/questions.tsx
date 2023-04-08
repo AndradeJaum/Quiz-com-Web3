3e94175c4c270ff87d1f04874804e73c416d4036
João Vitor de Andrade <andradejaum@gmail.com>
2023-04-07 22:15:07 -0300

    Initial Commit

---
+++ src/pages/questions.tsx
@@ -0,0 +1,62 @@
+export const questions = [
+  {
+    question: "Qual é o maior órgão do corpo humano?",
+    options: ["Fígado", "Coração", "Pele", "Cérebro"],
+    correctIndex: 2,
+  },
+  {
+    question: "Qual é a capital da Austrália?",
+    options: ["Melbourne", "Sydney", "Canberra", "Brisbane"],
+    correctIndex: 2,
+  },
+  {
+    question: "Quem escreveu a peça de teatro 'Romeu e Julieta'?",
+    options: [
+      "William Shakespeare",
+      "Charles Dickens",
+      "Jane Austen",
+      "Mark Twain",
+    ],
+    correctIndex: 0,
+  },
+  {
+    question: "Qual é o maior mamífero terrestre?",
+    options: ["Leão", "Elefante", "Rinoceronte", "Gorila"],
+    correctIndex: 1,
+  },
+  {
+    question: "Qual é o elemento químico mais abundante na crosta terrestre?",
+    options: ["Ferro", "Oxigênio", "Silício", "Carbono"],
+    correctIndex: 1,
+  },
+  {
+    question: "Qual é a capital do Canadá?",
+    options: ["Toronto", "Montreal", "Vancouver", "Ottawa"],
+    correctIndex: 3,
+  },
+  {
+    question: "Quem pintou a obra de arte 'Mona Lisa'?",
+    options: [
+      "Vincent van Gogh",
+      "Pablo Picasso",
+      "Claude Monet",
+      "Leonardo da Vinci",
+    ],
+    correctIndex: 3,
+  },
+  {
+    question: "Qual é o maior planeta do sistema solar?",
+    options: ["Marte", "Saturno", "Júpiter", "Vênus"],
+    correctIndex: 2,
+  },
+  {
+    question: "Quem foi o primeiro homem a pisar na Lua?",
+    options: ["Neil Armstrong", "Buzz Aldrin", "Yuri Gagarin", "Alan Shepard"],
+    correctIndex: 0,
+  },
+  {
+    question: "Qual é a fórmula química da água?",
+    options: ["H2SO4", "CO2", "H2O", "NaCl"],
+    correctIndex: 2,
+  },
+];
