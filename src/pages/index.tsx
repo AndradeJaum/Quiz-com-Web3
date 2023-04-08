6d7fd51e7d8e2d8092f172fd0382adc84bffdf20
João Vitor de Andrade <andradejaum@gmail.com>
2023-04-08 00:39:19 -0300

    add ternario + fix css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,11 +16,11 @@ export default function Home() {
   function handleNextQuestion() {
     // Lógica para verificar se a resposta está correta e fazer a transição para a próxima pergunta
     if (selectedOption === questions[currentQuestionIndex].correctIndex) {
-      console.log("correto")
+      console.log("correto");
       // Lógica para a resposta correta
     } else {
       // Lógica para a resposta incorreta
-      console.log("false")
+      console.log("false");
     }
     setSelectedOption(null);
     setCurrentQuestionIndex(currentQuestionIndex + 1);
@@ -49,49 +49,55 @@ export default function Home() {
             <p className="text-lg mb-4">
               Responda todas as perguntas corretamente para ganhar um token
             </p>
-            <button className="border-2 border-green text-green hover:bg-purpleHover hover:text-purple font-bold py-2 px-4 rounded">
+            <button
+              className="border-2 border-green text-green hover:bg-purpleHover hover:text-purple font-bold py-2 px-4 rounded"
+              onClick={handleStartQuiz}
+            >
               START
             </button>
           </div>
-
-          <div className="flex flex-col ">
-            <h2 className="text-3xl font-bold mb-4">RESPONDA</h2>
-            {currentQuestionIndex < questions.length ? (
-              <>
-                <p className="text-lg mb-4">
-                  {questions[currentQuestionIndex].question}
-                </p>
-                <ul className="flex flex-col gap-4">
-                  {questions[currentQuestionIndex].options.map(
-                    (option, index) => (
-                      <li
-                        key={index}
-                        className={`border border-green text-green hover:bg-purpleHover hover:text-purple font-bold py-2 px-4 rounded ${
-                          selectedOption === option
-                            ? "bg-purpleHover text-purple"
-                            : ""
-                        }`}
-                        onClick={() => handleOptionSelect(option)}
-                      >
-                        {option}
-                      </li>
-                    )
-                  )}
-                </ul>
-                <button
-                  className={`border-2 border-green text-green hover:bg-purpleHover hover:text-purple font-bold py-2 px-4 rounded mt-4 ${
-                    selectedOption ? "" : "opacity-50 cursor-not-allowed"
-                  }`}
-                  onClick={handleNextQuestion}
-                  disabled={!selectedOption}
-                >
-                  PRÓXIMA PERGUNTA
-                </button>
-              </>
-            ) : (
-              <p className="text-lg mb-4">Parabéns! Você concluiu o quiz.</p>
-            )}
-          </div>
+          {quizStarted == true ? (
+            <div className="flex flex-col ">
+              <h2 className="text-xl font-bold mb-4">Pergunta</h2>
+              {currentQuestionIndex < questions.length ? (
+                <>
+                  <p className="text-lg mb-4">
+                    {questions[currentQuestionIndex].question}
+                  </p>
+                  <ul className="flex flex-col gap-4">
+                    {questions[currentQuestionIndex].options.map(
+                      (option, index) => (
+                        <li
+                          key={index}
+                          className={`border border-green text-green hover:bg-purpleHover hover:text-purple font-bold py-2 px-4 rounded ${
+                            selectedOption === option
+                              ? "bg-purpleHover text-purple"
+                              : ""
+                          }`}
+                          onClick={() => handleOptionSelect(option)}
+                        >
+                          {option}
+                        </li>
+                      )
+                    )}
+                  </ul>
+                  <button
+                    className={`border-2 border-green text-green hover:bg-purpleHover hover:text-purple font-bold py-2 px-4 rounded mt-4 ${
+                      selectedOption ? "" : "opacity-50 cursor-not-allowed"
+                    }`}
+                    onClick={handleNextQuestion}
+                    disabled={!selectedOption}
+                  >
+                    PRÓXIMA PERGUNTA
+                  </button>
+                </>
+              ) : (
+                <p className="text-lg mb-4">Parabéns! Você concluiu o quiz.</p>
+              )}
+            </div>
+          ) : (
+            ""
+          )}
         </div>
       </section>
     </main>
